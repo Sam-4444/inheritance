252e1523e36b2b90e848576ade683fb8589a7adc
Sam <106349608+Sam-4444@users.noreply.github.com>
2022-08-31 19:56:52 +0430

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,11 +1,3 @@
-class mine:
-    x=44
-    def hello(name):
-        print("hello ",name)
-
-print(mine.x)
-mine.hello("Sam")
-
 class car:
     def __init__(self,name,brand):
         self.model_name=name
@@ -13,10 +5,10 @@ class car:
 
     def launch(self):
         print(f"{self.model_name} by {self.model_brand} launched !")
-
+# creating an object of car class
 car1=car("ghost","rolsroys ")
 print(car1.launch())
-
+# class benz inherited launch method from car class
 class benz(car):
     pass
 h1=benz("Maclaren","Benz")
